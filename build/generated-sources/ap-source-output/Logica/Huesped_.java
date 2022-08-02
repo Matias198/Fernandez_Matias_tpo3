355451d4061803fcde389eefc71f718689b17e43
Matias198 <matias.fernandez099@gmail.com>
2022-08-01 23:40:01 -0300

    v1.0

--- build/generated-sources/ap-source-output/Logica/Huesped_.java
+++ build/generated-sources/ap-source-output/Logica/Huesped_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-08-09T21:23:38")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-08-01T23:36:11")
 @StaticMetamodel(Huesped.class)
 public class Huesped_ extends Persona_ {
 
